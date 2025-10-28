1a1f938781dac111300b5775d73af027d8d3a6a7
Hung Nguyen <hungnguyen10897@gmail.com>
2025-10-28 19:56:45 +0200

    Remove packages

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,21 +1,21 @@
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
+// const morgan = require('morgan')
+// const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
-app.use(morgan(function (tokens, req, res) {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        req.method == "POST" ? JSON.stringify(req.body) : ""
-    ].join(' ')
-}))
+// app.use(morgan(function (tokens, req, res) {
+//     return [
+//         tokens.method(req, res),
+//         tokens.url(req, res),
+//         tokens.status(req, res),
+//         tokens.res(req, res, 'content-length'), '-',
+//         tokens['response-time'](req, res), 'ms',
+//         req.method == "POST" ? JSON.stringify(req.body) : ""
+//     ].join(' ')
+// }))
 
 persons = [
     {
