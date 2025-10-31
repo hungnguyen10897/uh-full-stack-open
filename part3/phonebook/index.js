3c6e9757cbcf2d69f512009584141bc6a31789db
Hung Nguyen <hungnguyen10897@gmail.com>
2025-10-31 14:33:06 +0200

    p3.22

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,80 +6,80 @@ const app = express()
 
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        req.method == "POST" ? JSON.stringify(req.body) : ""
-    ].join(' ')
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    req.method == 'POST' ? JSON.stringify(req.body) : ''
+  ].join(' ')
 }))
 app.use(express.static('dist'))
 
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (request, response) => {
-    const now = new Date();
-    getAll(persons => response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${now.toUTCString()}</p>`))
+  const now = new Date()
+  getAll(persons => response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${now.toUTCString()}</p>`))
 })
 
 
 app.get('/api/persons', (request, response) => {
-    getAll().then(persons => response.json(persons))
+  getAll().then(persons => response.json(persons))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    findById(request.params.id).then(person => {
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    })
-        .catch(error => next(error))
+  findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    deletePersonById(request.params.id).then(result => response.status(204).end()).catch(error => next(error))
+  deletePersonById(request.params.id).then(() => response.status(204).end()).catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const person = request.body
-    if (!person.name || !person.number) {
-        return response.status(400).send('Missing attribute')
-    }
+  const person = request.body
+  if (!person.name || !person.number) {
+    return response.status(400).send('Missing attribute')
+  }
 
-    // searchedPerson = persons.find(item => item.name === person.name)
-    // if (searchedPerson) {
-    //     return response.status(400).send('Duplicate')
-    // }
+  // searchedPerson = persons.find(item => item.name === person.name)
+  // if (searchedPerson) {
+  //     return response.status(400).send('Duplicate')
+  // }
 
-    addPerson(person).then(addedPerson => response.json(addedPerson)).catch(error => next(error))
+  addPerson(person).then(addedPerson => response.json(addedPerson)).catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response) => {
-    const person = request.body
-    if (!person.number) {
-        return response.status(400).send('Missing number')
-    }
+  const person = request.body
+  if (!person.number) {
+    return response.status(400).send('Missing number')
+  }
 
-    updatePersonById(request.params.id, person)
+  updatePersonById(request.params.id, person)
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 // this has to be the last loaded middleware, also all the routes should be registered before this!
@@ -87,5 +87,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -7,55 +7,55 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    number: String,
+  name: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  number: String,
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 const findById = (id) => {
-    return Person.findById(id)
+  return Person.findById(id)
 }
 
 const getAll = () => {
-    return Person.find({})
+  return Person.find({})
 }
 
 const addPerson = ({ name, number }) => {
-    const person = new Person({
-        name: name,
-        number: number,
-    })
+  const person = new Person({
+    name: name,
+    number: number,
+  })
 
-    return person.save()
+  return person.save()
 }
 
 const deletePersonById = (id) => {
-    return Person.findByIdAndDelete(id)
+  return Person.findByIdAndDelete(id)
 }
 
 const updatePersonById = (id, person) => {
-    return Person.findByIdAndUpdate(id, person)
+  return Person.findByIdAndUpdate(id, person)
 }
 
 module.exports = { getAll, findById, addPerson, deletePersonById, updatePersonById }
