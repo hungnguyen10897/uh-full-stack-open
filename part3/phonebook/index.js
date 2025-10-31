0b1f77583859058d8a8c5077ecbd7cdf0281261e
Hung Nguyen <hungnguyen10897@gmail.com>
2025-10-31 14:15:11 +0200

    uibuild

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -47,18 +47,18 @@ app.delete('/api/persons/:id', (request, response, next) => {
     deletePersonById(request.params.id).then(result => response.status(204).end()).catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const person = request.body
     if (!person.name || !person.number) {
         return response.status(400).send('Missing attribute')
     }
 
-    searchedPerson = persons.find(item => item.name === person.name)
-    if (searchedPerson) {
-        return response.status(400).send('Duplicate')
-    }
+    // searchedPerson = persons.find(item => item.name === person.name)
+    // if (searchedPerson) {
+    //     return response.status(400).send('Duplicate')
+    // }
 
-    addPerson(person).then(addedPerson => response.json(addedPerson))
+    addPerson(person).then(addedPerson => response.json(addedPerson)).catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response) => {
@@ -75,6 +75,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
