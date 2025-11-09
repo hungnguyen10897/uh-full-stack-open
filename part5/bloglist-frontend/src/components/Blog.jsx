e1e6d66cb94fb71c3fac752de2f15236b0794dfa
Hung Nguyen <hungnguyen10897@gmail.com>
2025-11-09 17:56:08 +0200

    p5.5-5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,42 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  if (!visible) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}{" "}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+    );
+  }
+
+  return (
+    <div style={blogStyle}>
+      <p>
+        {blog.title}
+        <button onClick={toggleVisibility}>hide</button>
+      </p>
+      <p>{blog.url}</p>
+      <p>
+        likes {blog.likes} <button>like</button>
+      </p>
+      <p>{blog.author}</p>
+    </div>
+  );
+};
+
+export default Blog;
